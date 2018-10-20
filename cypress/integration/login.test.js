f0571d93be48421c346809d53efc36360df14470
Marc Brown <heymarcbrown@gmail.com>
2018-10-20 16:19:38 -0400

    [e2e] Test formatting

--- cypress/integration/login.test.js
+++ cypress/integration/login.test.js
@@ -8,52 +8,47 @@ const password = 'thebestpassword';
 describe('Login', () => {
 
     it('should display the sign in form', () => {
-        cy
-        .visit('/login')
-        .get('h1').contains('Login')
-        .get('form')
-        .get('input[disabled]')
-        .get('.validation-list')
-        .get('.validation-list > .error').first().contains('Email is required.');
+        cy.visit('/login')
+          .get('h1').contains('Login')
+          .get('form')
+          .get('input[disabled]')
+          .get('.validation-list')
+          .get('.validation-list > .error').first().contains('Email is required.');
     });
 
     it('should allow a user to sign in', () => {
         // register user
-        cy
-        .visit('/register')
-        .get('input[name="username"]').type(username)
-        .get('input[name="email"]').type(email)
-        .get('input[name="password"]').type(password)
-        .get('input[type="submit"]').click();
+        cy.visit('/register')
+          .get('input[name="username"]').type(username)
+          .get('input[name="email"]').type(email)
+          .get('input[name="password"]').type(password)
+          .get('input[type="submit"]').click();
 
         // log a user out
         cy.get('.navbar-burger').click();
         cy.contains('Log Out').click();
 
         // log a user in
-        cy
-        .get('a').contains('Log In').click()
-        .get('input[name="email"]').type(email)
-        .get('input[name="password"]').type(password)
-        .get('input[type="submit"]').click()
-        .wait(100);
+        cy.get('a').contains('Log In').click()
+          .get('input[name="email"]').type(email)
+          .get('input[name="password"]').type(password)
+          .get('input[type="submit"]').click()
+          .wait(100);
 
         // assert user is redirected to '/'
         // assert '/' is displayed properly
         cy.get('.navbar-burger').click();
         cy.contains('All Users');
-        cy
-        .get('table')
-        .find('tbody > tr').last()
-        .find('td').contains(username);
+        cy.get('table')
+          .find('tbody > tr').last()
+          .find('td').contains(username);
         cy.get('.notification.is-success').contains('Welcome!');
         cy.get('.navbar-burger').click();
         cy.get('.navbar-menu').within(() => {
-            cy
-            .get('.navbar-item').contains('User Status')
-            .get('.navbar-item').contains('Log Out')
-            .get('.navbar-item').contains('Log In').should('not.be.visible')
-            .get('.navbar-item').contains('Register').should('not.be.visible');
+            cy.get('.navbar-item').contains('User Status')
+              .get('.navbar-item').contains('Log Out')
+              .get('.navbar-item').contains('Log In').should('not.be.visible')
+              .get('.navbar-item').contains('Register').should('not.be.visible');
         });
 
         // log a user out
@@ -62,11 +57,10 @@ describe('Login', () => {
         // assert '/logout' is displayed properly
         cy.get('p').contains('You are now logged out');
         cy.get('.navbar-menu').within(() => {
-            cy
-            .get('.navbar-item').contains('User Status').should('not.be.visible')
-            .get('.navbar-item').contains('Log Out').should('not.be.visible')
-            .get('.navbar-item').contains('Log In')
-            .get('.navbar-item').contains('Register');
+            cy.get('.navbar-item').contains('User Status').should('not.be.visible')
+              .get('.navbar-item').contains('Log Out').should('not.be.visible')
+              .get('.navbar-item').contains('Log In')
+              .get('.navbar-item').contains('Register');
         });
     });
 
@@ -84,37 +78,32 @@ describe('Login', () => {
         cy.contains('Login');
         cy.get('.navbar-burger').click();
         cy.get('.navbar-menu').within(() => {
-            cy
-            .get('.navbar-item').contains('User Status').should('not.be.visible')
-            .get('.navbar-item').contains('Log Out').should('not.be.visible')
-            .get('.navbar-item').contains('Log In')
-            .get('.navbar-item').contains('Register');
+            cy.get('.navbar-item').contains('User Status').should('not.be.visible')
+              .get('.navbar-item').contains('Log Out').should('not.be.visible')
+              .get('.navbar-item').contains('Log In')
+              .get('.navbar-item').contains('Register');
         });
-        cy
-        .get('.notification.is-success').should('not.be.visible')
-        .get('.notification.is-danger').contains('Login failed.');
+        cy.get('.notification.is-success').should('not.be.visible')
+          .get('.notification.is-danger').contains('Login failed.');
 
         // attempt to log in
-        cy
-        .get('a').contains('Log In').click()
-        .get('input[name="email"]').type(email)
-        .get('input[name="password"]').type('incorrectpassword')
-        .get('input[type="submit"]').click()
-        .wait(100);
+        cy.get('a').contains('Log In').click()
+          .get('input[name="email"]').type(email)
+          .get('input[name="password"]').type('incorrectpassword')
+          .get('input[type="submit"]').click()
+          .wait(100);
 
         // assert user login failed
         cy.contains('All Users').should('not.be.visible');
         cy.contains('Login');
         cy.get('.navbar-burger').click();
         cy.get('.navbar-menu').within(() => {
-            cy
-            .get('.navbar-item').contains('User Status').should('not.be.visible')
-            .get('.navbar-item').contains('Log Out').should('not.be.visible')
-            .get('.navbar-item').contains('Log In')
-            .get('.navbar-item').contains('Register');
+            cy.get('.navbar-item').contains('User Status').should('not.be.visible')
+              .get('.navbar-item').contains('Log Out').should('not.be.visible')
+              .get('.navbar-item').contains('Log In')
+              .get('.navbar-item').contains('Register');
         });
-        cy
-        .get('.notification.is-success').should('not.be.visible')
-        .get('.notification.is-danger').contains('Login failed.');
+        cy.get('.notification.is-success').should('not.be.visible')
+          .get('.notification.is-danger').contains('Login failed.');
     });
 });
--- cypress/integration/status.test.js
+++ cypress/integration/status.test.js
@@ -8,24 +8,22 @@ const password = 'thebestpassword';
 describe('Status', () => {
 
     it('should not display user info if a user is not logged in', () => {
-        cy
-        .visit('/status')
-        .get('p').contains('You must be logged in to view this.')
-        .get('a').contains('User Status').should('not.be.visible')
-        .get('a').contains('Log Out').should('not.be.visible')
-        .get('a').contains('Register')
-        .get('a').contains('Log In');
+        cy.visit('/status')
+          .get('p').contains('You must be logged in to view this.')
+          .get('a').contains('User Status').should('not.be.visible')
+          .get('a').contains('Log Out').should('not.be.visible')
+          .get('a').contains('Register')
+          .get('a').contains('Log In');
     });
 
     it('should display user info if a user is logged in', () => {
         // register user
-        cy
-        .visit('/register')
-        .get('input[name="username"]').type(username)
-        .get('input[name="email"]').type(email)
-        .get('input[name="password"]').type(password)
-        .get('input[type="submit"]').click()
-        .get('.navbar-burger').click();
+        cy.visit('/register')
+          .get('input[name="username"]').type(username)
+          .get('input[name="email"]').type(email)
+          .get('input[name="password"]').type(password)
+          .get('input[type="submit"]').click()
+          .get('.navbar-burger').click();
 
         cy.wait(1000);
 
@@ -34,15 +32,14 @@ describe('Status', () => {
         cy.get('.navbar-burger').click();
         cy.contains('User Status').click();
         
-        cy
-        .get('li > strong').contains('User ID:')
-        .get('li > strong').contains('Email:')
-        .get('li').contains(email)
-        .get('li > strong').contains('Username:')
-        .get('li').contains(username)
-        .get('a').contains('User Status')
-        .get('a').contains('Log Out')
-        .get('a').contains('Register').should('not.be.visible')
-        .get('a').contains('Log In').should('not.be.visible');
+        cy.get('li > strong').contains('User ID:')
+          .get('li > strong').contains('Email:')
+          .get('li').contains(email)
+          .get('li > strong').contains('Username:')
+          .get('li').contains(username)
+          .get('a').contains('User Status')
+          .get('a').contains('Log Out')
+          .get('a').contains('Register').should('not.be.visible')
+          .get('a').contains('Log In').should('not.be.visible');
     });
 });
