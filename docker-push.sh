1dbf7042da4f8e8a12912bdb2f8b435a20355791
Marc Brown <heymarcbrown@gmail.com>
2018-10-23 14:46:38 -0400

    Fix awscli download link

--- docker-push.sh
+++ docker-push.sh
@@ -11,7 +11,7 @@ then
 
     if [ "$TRAVIS_BRANCH" == "ecr" ] || [ "$TRAVIS_BRANCH" == "staging" ] || [ "$TRAVIS_BRANCH" == "production" ]
     then
-        curl "https://s3.amazon.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+        curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 
         unzip awscli-bundle.zip
         ./awscli-bundle/install -b ~/bin/aws
