8926a32e11f9dc6c02bf4f990b5e24c65bda18e8
Marc Brown <heymarcbrown@gmail.com>
2018-11-08 17:38:49 -0500

    Adds ECS staging task definitions

---
+++ docker-deploy-staging.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]
+then
+
+  if [ "$TRAVIS_BRANCH" == "staging" ]
+  then
+
+    JQ="jq --raw-output --exit-status"
+
+    configure_aws_cli() {
+        aws --version
+        aws configure set default.region us-east-1
+        aws configure set default.output json
+        echo "AWS Configured!"
+    }
+
+    register_definition() {
+      if revision=$(aws ecs register-task-definition --cli-input-json "$task_def" | $JQ '.taskDefinition.taskDefinitionArn'); then
+        echo "Revision: $revision"
+      else
+        echo "Failed to register task definition"
+        return 1
+      fi
+    }
+
+    update_service() {
+      if [[ $(aws ecs update-service --cluster $cluster --service $service --task-definition $revision | $JQ '.service.taskDefinition') != $revision ]]; then
+        echo "Error updating service."
+        return 1
+      fi
+    }
+
+    deploy_cluster() {
+
+      cluster="codecheck-staging-cluster"
+
+      # users
+      service="codecheck-users-staging-service"
+      template="ecs_users_staging_taskdefinition.json"
+      task_template=$(cat "ecs/$template")
+      task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $AWS_ACCOUNT_ID)
+      echo "$task_def"
+      register_definition
+      update_service
+
+      # web
+      service="codecheck-web-staging-service"
+      template="ecs_web_staging_taskdefinition.json"
+      task_template=$(cat "ecs/$template")
+      task_def=$(printf "$task_template" $AWS_ACCOUNT_ID)
+      echo "$task_def"
+      register_definition
+      update_service
+
+      # swagger
+      service="codecheck-swagger-staging-service"
+      template="ecs_swagger_staging_taskdefinition.json"
+      task_template=$(cat "ecs/$template")
+      task_def=$(printf "$task_template" $AWS_ACCOUNT_ID)
+      echo "$task_def"
+      register_definition
+      update_service
+
+    }
+
+    configure_aws_cli
+    deploy_cluster
+
+  fi
+
+fi
