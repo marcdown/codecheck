57e55e91ce65a819819098c963a6167bff616f20
Marc Brown <heymarcbrown@gmail.com>
2018-11-15 12:05:12 -0500

    Adds Exercises base component and tests

---
+++ services/web/src/components/Exercises.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Exercises extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {};
+    };
+
+    render() {
+        return (
+            <div>
+                <p>Hello, World!</p>
+            </div>
+        )
+    };
+};
+
+export default Exercises;
