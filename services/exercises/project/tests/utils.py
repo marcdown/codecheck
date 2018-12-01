533e83a31086567026dcb07b439f119c78e76678
Marc Brown <marcdown@users.noreply.github.com>
2018-12-01 16:13:22 -0500

    [exercises] Exercises API (GET/POST) (#27)

--- services/exercises/project/tests/utils.py
+++ services/exercises/project/tests/utils.py
@@ -3,8 +3,7 @@ from project.api.models import Exercise
 
 
 def add_exercise(
-        body=('Define a function called sum that takes two integers as '
-              'arguments and returns their sum'),
+        body='Define a function called sum that takes two integers as arguments and returns their sum',
         test_code='sum(2, 2)',
         test_code_solution='4'):
     exercise = Exercise(
