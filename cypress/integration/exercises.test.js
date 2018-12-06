d99b3e13deafae23c213f0d8578859be3b4cbe1e
Marc Brown <heymarcbrown@gmail.com>
2018-12-06 15:11:45 -0500

    [web] Exercises AJAX refactor

--- cypress/integration/exercises.test.js
+++ cypress/integration/exercises.test.js
@@ -40,10 +40,14 @@ describe('Exercises', () => {
       .get('button.button.is-primary').contains('Run Code');
 
     // assert user can submit an exercise
+    for (let i = 0; i < 23; i++) {
+        cy.get('textarea').type('{backspace}', { force: true })
+    }
     cy
+      .get('textarea').type('def sum(x,y):\nreturn x+y', { force: true })
       .get('button').contains('Run Code').click()
       .wait('@gradeExercise')
-      .get('h5 > .grade-text').contains('Incorrect!');
+      .get('h5 > .grade-text').contains('Correct!');
 
   });
 
