26d774fb85a34c4a0f28bc163dc668b012111dc3
Marc Brown <marcdown@users.noreply.github.com>
2018-12-08 12:03:21 -0500

    [web] Add prev/next buttons to Exercise component (#36)

--- services/web/src/components/Exercises.jsx
+++ services/web/src/components/Exercises.jsx
@@ -8,6 +8,7 @@ class Exercises extends Component {
     constructor (props) {
         super(props)
         this.state = {
+            currentExercise: 0,
             exercises: [],
             editor: {
                 value: '# Enter your code here.',
@@ -15,10 +16,19 @@ class Exercises extends Component {
                 showGrading: false,
                 showCorrect: false,
                 showIncorrect: false
-            }
+            },
+            showButtons: {
+                prev: false,
+                next: false
+            },
         };
         this.onChange = this.onChange.bind(this);
+        this.renderButtons = this.renderButtons.bind(this);
         this.submitExercise = this.submitExercise.bind(this);
+        this.updateScore = this.updateScore.bind(this);
+        this.nextExercise = this.nextExercise.bind(this);
+        this.prevExercise = this.prevExercise.bind(this);
+        this.resetEditor = this.resetEditor.bind(this);
     };
 
     onChange(value) {
@@ -27,40 +37,121 @@ class Exercises extends Component {
         this.setState(newState);
     };
 
+    renderButtons() {
+        const index = this.state.currentExercise;
+        let nextButton = false;
+        let prevButton = false;
+        if (typeof this.state.exercises[index + 1] !== 'undefined') {
+          nextButton = true;
+        }
+        if (typeof this.state.exercises[index - 1] !== 'undefined') {
+          prevButton = true;
+        }
+        this.setState({
+          showButtons: {
+            next: nextButton,
+            prev: prevButton
+          }
+        });
+    };
+
     getExercises() {
         axios.get(`${process.env.REACT_APP_EXERCISES_SERVICE_URL}/exercises`)
-        .then((res) => { this.setState({ exercises: res.data.data.exercises }); })
+        .then((res) => {
+            this.setState({
+                exercises: res.data.data.exercises,
+                currentExercise: 0
+            });
+            this.renderButtons();
+        })
         .catch((err) => { console.log(err); });
     };
 
     submitExercise(event, id) {
         event.preventDefault();
         const newState = this.state.editor;
-        const exercise = this.state.exercises.filter(el => el.id === id)[0];
+        const exercise = this.state.exercises.filter(el => el.id === id)[0]
         newState.showGrading = true;
         newState.showCorrect = false;
         newState.showIncorrect = false;
         newState.button.isDisabled = true;
         this.setState(newState);
-        const data = { 
-            answer: this.state.editor.value,
-            test: exercise.test_code,
-            solution: exercise.test_code_solution
+        const data = {
+          answer: this.state.editor.value,
+          test: exercise.test_code,
+          solution: exercise.test_code_solution,
         };
         const url = process.env.REACT_APP_API_GATEWAY_URL;
         axios.post(url, data)
         .then((res) => {
-            newState.showGrading = false
-            newState.button.isDisabled = false
-            if (res.data && !res.data.errorType) { newState.showCorrect = true };
-            if (!res.data || res.data.errorType) { newState.showIncorrect = true };
-            this.setState(newState);
+          newState.showGrading = false
+          newState.button.isDisabled = false
+          // new
+          if (res.data && !res.data.errorType) {
+            newState.showCorrect = true
+            this.updateScore(exercise.id, true)
+          };
+          // new
+          if (!res.data || res.data.errorType) {
+            newState.showIncorrect = true
+            this.updateScore(exercise.id, false)
+          };
+          this.setState(newState);
         })
         .catch((err) => {
-            newState.showGrading = false
-            newState.button.isDisabled = false
-            console.log(err);
+          newState.showGrading = false
+          newState.button.isDisabled = false
+          console.log(err);
+          this.updateScore(exercise.id, false)
         })
+    };
+
+    updateScore(exerciseID, bool) {
+        const options = {
+          url: `${process.env.REACT_APP_SCORES_SERVICE_URL}/scores/${exerciseID}`,
+          method: 'put',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${window.localStorage.authToken}`
+          },
+          data: {correct:bool}
+        };
+        return axios(options)
+        .then((res) => { console.log(res); })
+        .catch((error) => { console.log(error); });
+    };
+
+    nextExercise() {
+        if (this.state.showButtons.next) {
+          const currentExercise = this.state.currentExercise;
+          this.setState({currentExercise: currentExercise + 1}, () => {
+            this.resetEditor()
+            this.renderButtons();
+          });
+        }
+    };
+
+    prevExercise() {
+        if (this.state.showButtons.prev) {
+          const currentExercise = this.state.currentExercise;
+          this.setState({currentExercise: currentExercise - 1}, () => {
+            this.resetEditor()
+            this.renderButtons();
+          });
+        }
+    };
+
+    resetEditor() {
+        const editor = {
+          value: '# Enter your code here.',
+          button: {
+            isDisabled: false,
+          },
+          showGrading: false,
+          showCorrect: false,
+          showIncorrect: false,
+        }
+        this.setState({editor: editor});
     };
 
     componentDidMount() {
@@ -79,13 +170,22 @@ class Exercises extends Component {
                 }
                 {this.state.exercises.length > 0 &&
                     <Exercise
-                    exercise={this.state.exercises[0]}
-                    editor={this.state.editor}
-                    isAuthenticated={this.props.isAuthenticated}
-                    onChange={this.onChange}
-                    submitExercise={this.submitExercise}
+                        exercise={this.state.exercises[this.state.currentExercise]}
+                        editor={this.state.editor}
+                        isAuthenticated={this.props.isAuthenticated}
+                        onChange={this.onChange}
+                        submitExercise={this.submitExercise}
                     />
                 }
+                <div className="field is-grouped">
+                    { this.state.showButtons.prev &&
+                        <button className="button is-info" onClick={() => this.prevExercise()}>&lt; Prev</button>
+                    }
+                    &nbsp;
+                    { this.state.showButtons.next &&
+                        <button className="button is-info" onClick={() => this.nextExercise()}>Next &gt;</button>
+                    }
+                </div>
             </div>
         )
     };
